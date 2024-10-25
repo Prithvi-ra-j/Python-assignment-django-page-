4983321c988f088591fd2de941369b0f413ae097
SherlOckh-oLmes <46382945+SherlOckh-oLmes@users.noreply.github.com>
2024-10-26 02:02:15 +0530

    Added a website link to the messge in the email

--- upload/views.py
+++ upload/views.py
@@ -21,6 +21,7 @@ def upload_file(request):
 
                 github_link = "https://github.com/Prithvi-ra-j/Python-assignment-django-page-"
                 summary = df.describe().to_string()
+                website="https://python-assignment-django-page.onrender.com/"
                 message = f"{summary}\n\nGitHub Repository: {github_link}"
                 
                 send_mail(
