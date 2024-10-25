600a31db923809f374c26423ad075c651a5a7c7a
SherlOckh-oLmes <46382945+SherlOckh-oLmes@users.noreply.github.com>
2024-10-26 01:47:52 +0530

    Map root URL to upload_file view

--- DevTest/urls.py
+++ DevTest/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
+from upload import views as upload_views 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('upload/', include('upload.urls')),
-]
+    path('', upload_views.upload_file, name='home'),
+    ]
